8c8c946cceee618034afbb3cd47eda94b7631bb0
aaidenplays <aaidenplays@yahoo.com>
2020-04-21 16:15:25 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,46 @@
-export function managePresents(state, action){
+export function managePresents (state={
+    numberOfPresents: 0
+},action){
+    switch(action.type){
+        case "INCREASE":
+            return Object.assign({}, state, {
+                numberOfPresents: state.numberOfPresents + 1
+            })
+            default:
+                return state;
+    }
+
 }
+
+
+//   state = {
+//     numberOfPresents: 0
+//   },
+//   action
+// ) {
+//   switch (action.type) {
+//     case 'INCREASE':
+//       return Object.assign({}, state, {
+//         numberOfPresents: state.numberOfPresents + 1
+//       })
+//       default: return state
+//   }
+
+//   {
+//         numberOfPresents: 0
+//     }
+//     action = {
+//         type: "INCREASE"
+//     }
+//     action = {
+//         type: "ADD_FRIEND",
+//         friend: {
+//             name: "Chrome Boi",
+//             hometown: "NYC",
+//             id: 1
+//         }
+//     }
+//     action = {
+//         type: "REMOVE_FRIEND",
+//         id: 1
+//     }
